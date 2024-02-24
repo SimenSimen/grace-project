ba9fbea4ec887e78435bd8fd85c8359a13e0e3a5
simen <de>
2024-02-24 13:55:42 +0800

    fxied

--- src/components/Products/shampoo-txt/Shampoo.vue
+++ src/components/Products/shampoo-txt/Shampoo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="product-desc-type-1">
-    <h1 class="font-bold text-2xl lg:text-4xl text-blue-700">艾絲朵甦活護髮膜</h1>
+    <h1 class="font-bold text-2xl lg:text-4xl text-blue-700">艾絲朵甦活洗髮精</h1>
 
     <div class="text-gray-500 mt-4">
       <h3 class="font-bold text-lg mb-2 text-blue-700 flex items-center">
@@ -8,22 +8,21 @@
         <span>產品特性</span>
       </h3>
 
-      <p>✓ 髮絲形成一層保護膜，防止秀髮打結，柔順好梳理</p>
-      <p>✓ 馴服各種不羈的髮絲，使頭髮呈現豐盈感</p>
-      <p>✓ 補充滋養因子讓秀髮更強健</p>
-      <p>✓ 喚醒毛囊，重新煥發活力，讓髮根呼吸</p>
-      <p class="mb-4">✓ 提升秀髮柔軟觸感，呈現光澤亮麗</p>
+      <p>✓ 含有天然認證 COSMOS 的界面活性劑成分</p>
+      <p>✓ 細膩泡沫可溫和清潔不刺激，帶走油脂與髒汙</p>
+      <p>✓ 補充滋養因子，喚醒毛囊，讓髮根呼吸</p>
+      <p>✓ 喚醒毛囊，平衡秀髮油水含量</p>
+      <p class="mb-4">✓ 由內而外呵護每根髮絲，使秀髮輕盈滑順</p>
 
       <h3 class="font-bold text-lg mb-2 text-blue-700 flex items-center">
         <div class="text-bar mr-1"></div>
         <span>成分</span>
       </h3>
 
-      <p>NMN 煙酰胺單核苷酸-延緩頭肌衰老，強化滋養毛囊</p>
-      <p>cMiracle 外泌體 / 幹細胞-強健髮根，活絡毛髮，提升頭皮保護健康</p>
-      <p class="mb-4">
-        TMS50 22碳四級銨鹽-天然植物萃取，調理頭髮靜電失衡，易於梳理性，防止毛躁感，增加柔軟性觸感
-      </p>
+      <p class="mb-2">NMN 煙酰胺單核苷酸-延緩頭肌衰老，強化滋養毛囊</p>
+      <p class="mb-2">cMiracle 外泌體 / 幹細胞-強健髮根，活絡毛髮，提升頭皮保護健康</p>
+      <p class="mb-2">MAB 醋酸鹽類界面活性劑-COSMOS 認證，溫和不刺激，豐富細膩、柔滑的泡沫</p>
+      <p class="mb-4">AOS35 陰離子界面活性劑-豐富高泡沫，帶走油脂和髒汙</p>
 
       <h3 class="font-bold text-lg mb-2 text-blue-700 flex items-center">
         <div class="text-bar mr-1"></div>
@@ -46,7 +45,7 @@
       <p>容量：500ml</p>
       <p>保存期限：3 年</p>
       <p>保存方法：請置於陰涼處</p>
-      <p>使用方法：使用洗髮精後，取適量護髮膜按摩於頭髮上，特別於受損處，接著沖淨即可。</p>
+      <p>使用方法：頭髮沖濕後，將洗髮精倒入手中揉搓起泡，接著溫水沖淨即可。</p>
       <p>注意事項：本產品僅提供頭髮使用，如不慎入眼，立即沖水。</p>
       <p>MADE IN TAIWAN</p>
     </div>
